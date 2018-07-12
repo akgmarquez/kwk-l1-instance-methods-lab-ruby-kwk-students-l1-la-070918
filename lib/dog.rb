a287077ddb11e1bab1278056af1142f6de65b760
akgmarquez <akgmarquez@gmail.com>
2018-07-12 21:53:15 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+# dog.rb
+class Dogs
+  def initialize (bark, sit)
+    @bark = bark
+    @sit = sit
+end
+
+  def bark
+    @bark
+  if bark
+    puts "Woof!"
+end
+
+  def sit
+  @sit
+  end
+end
